e08c3e3bd68efe499955719aeb8e1194462fafbc
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-05-16 22:13:35 +0600

    d

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os,sys
 import schedule
 from flask import Flask, request
-from response import response
+from response import message_response
 from pymessenger import Bot
 
 app = Flask("My echo bot")
